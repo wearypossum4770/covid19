bce5c1d76d130fdb748829769519fa89e46ef6f2
wearypossum4770 <wearypossum4770@yahoo.com>
2020-11-07 16:51:17 -0500

    deploy to production

--- covid/settings.py
+++ covid/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -137,3 +137,5 @@ LOGIN_URL = "login"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# django_heroku.settings(locals())
