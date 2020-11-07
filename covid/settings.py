b54af11dd0bb15d01871c0cd9568cb622ec1b2bc
wearypossum4770 <wearypossum4770@yahoo.com>
2020-11-07 16:35:11 -0500

    updating repository

--- covid/settings.py
+++ covid/settings.py
@@ -32,7 +32,9 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = [
+       
     "django.middleware.security.SecurityMiddleware",
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -127,9 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS  = [BASE_DIR /'static',]
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 LOGIN_URL = "login"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
